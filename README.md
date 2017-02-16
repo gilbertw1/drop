9ebcd3ca86e3a2a6a2c2e80b7d4b2140d0ba308a
Bryan Gilbert <gilbertw1@gmail.com>
2017-02-16 13:54:47 -0500

    initial commit of rough drop implementation

---
+++ README.md
@@ -0,0 +1,97 @@
+drop
+====
+
+A simple screenshot tool with S3 support written in rust.
+
+
+Dependencies
+------------
+
+* slop - https://github.com/naelstrof/slop
+* imagemagick - https://www.imagemagick.org
+* s3cmd - http://s3tools.org/s3cmd
+* xsel - http://www.vergenet.net/%7Econrad/software/xsel/
+
+
+Installation
+------------
+
+Clone this repo
+
+    git clone git@github.com:gilbertw1/drop.git
+
+Place ```config.default``` in ```~/.config/drop/config``` and edit it with required values
+
+    mkdir -p ~/.config/drop
+    cp config.default ~/.config/drop/config
+    
+Build drop:
+
+    cargo build --release
+  
+Run drop:
+
+    ./target/release/drop
+
+Usage
+-----
+
+### Take a screenshot
+
+Drop can be used to take a screenshot and allows you to select a portion of the screen to save and upload. Additionally, a single window can be screenshotted by simply clicking on the window. Once the screenshot has been saved and upload a notification will popup saying the screenshot has been upload and a url to the screenshot will be saved in the clipboard.
+
+Run:
+
+    ./drop
+    
+
+Configuration
+-------------
+
+The drop configuration file should be placed at ```~/.config/drop/config```. It's values are as follows:
+
+```toml
+    [drop]
+    dir = '~/.drop'  # Directory used to save screenshots
+    host = ''        # OPTIONAL - Custom domain used to generate screenshot links
+    
+    [aws]
+    bucket = 'drop'  # OPTIONAL - S3 bucket to upload screenshots & files to
+    key = ''         # OPTIONAL - AWS credentials used to authenticate with S3
+    secret = ''      # OPTIONAL - AWS secret used to authenticate with S3
+```
+
+* More info on aws access keys [here](https://aws.amazon.com/developers/access-keys/)
+* If bucket, key, or secret is missing drop will only save the screenshot locally
+
+S3 Setup
+--------
+
+If configuring Drop to upload to S3 a bucket will be required. Info on creating S3 buckets can be found here: http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html. Once a bucket has been created, if you want the drops to be publicly accessible the following policy should be added to the bucket:
+
+    {
+      "Version": "2008-10-17",
+      "Statement": [{
+        "Sid": "AllowPublicRead",
+        "Effect": "Allow",
+        "Principal": { "AWS": "*" },
+        "Action": ["s3:GetObject"],
+        "Resource": ["arn:aws:s3:::BUCKET_NAME/*" ]
+      }]
+    }
+    
+* Replace ```bucket``` with the name of your bucket.
+
+
+Custom Domain Setup
+-------------------
+
+In order for Drop to work correctly with a custom domain, a few additional steps need to be taken.
+
+* Create an S3 bucket with the name of the custom domain. For example if I want my drops available at ```drop.mydomain.com```, then the S3 bucket I create needs to be named ```drop.mydomain.com```.
+
+* Enable static website hosting for the bucket. This can be done by selecting the bucket and checking "Enable website hosting" under the "Static Website Hosting" section in the bucket properties. Note that you will need at least a blank html file in the bucket to select as the required 'Index Document'.
+
+* Add a cname record to your DNS pointing to the static S3 website endpoint (this can be found in the "Static Website Hosting" properties section). In the above example, in the DNS settings for ```mydomain.com```, a cname entry for ```drop``` would be added pointing to the static S3 site.
+
+* Update the ```~/.config/drop/config``` file with the variable ```host``` pointing to your custom domain (```drop.mydomain.com```)
---
+++ src/main.rs
@@ -0,0 +1,129 @@
+extern crate config;
+extern crate rand;
+
+use std::env;
+use std::string::String;
+use std::collections::HashMap;
+use std::process::{Command, Stdio};
+use std::path::PathBuf;
+use std::io::Write;
+use rand::Rng;
+
+fn main() {
+  let config = load_config();
+  std::fs::create_dir_all(&config.drop_dir);
+  let out_filename = gen_filename("png");
+  let mut out_file_path = PathBuf::from(&config.drop_dir);
+  out_file_path.push(&out_filename);
+  let slop_result = run_slop();
+
+  if slop_result.cancel {
+    println!("Cancelled drop, exiting.");
+    std::process::exit(1);
+  }
+
+  take_and_crop_screenshot(&slop_result, &out_file_path);
+
+  if config.aws_bucket.is_none() || config.aws_key.is_none() || config.aws_secret.is_none() {
+    println!("AWS not properly defined, not uploading.");
+    copy_to_clipboard(format!("file://{}", out_file_path.to_string_lossy().into_owned()));
+    send_notification(&out_file_path);
+  }
+
+  upload_file_to_s3(&config, &out_file_path);
+
+  let url = create_drop_url(&config, out_filename);
+  copy_to_clipboard(url);
+  send_notification(&out_file_path);
+}
+
+fn gen_filename(ext: &str) -> String {
+  let rand_filename: String = rand::thread_rng().gen_ascii_chars().take(10).collect();
+  format!("{}.{}", rand_filename, ext)
+}
+
+fn load_config() -> DropConfig {
+  let home_dir = env::home_dir().unwrap();
+  let mut conf = config::Config::new();
+  let conf_file = home_dir.join(".config/drop/config").to_string_lossy().into_owned();
+  conf.merge(config::File::new(conf_file.as_str(), config::FileFormat::Toml)).unwrap();
+  DropConfig {
+    drop_dir: conf.get("drop.dir").unwrap().into_str().unwrap().replace("~", &home_dir.to_string_lossy().into_owned()),
+    drop_host: conf.get("drop.host").map(|host| host.into_str().unwrap()),
+    aws_bucket: conf.get("aws.bucket").map(|bucket| bucket.into_str().unwrap()),
+    aws_key: conf.get("aws.key").map(|key| key.into_str().unwrap()),
+    aws_secret: conf.get("aws.secret").map(|secret| secret.into_str().unwrap()),
+  }
+}
+
+fn run_slop() -> SlopResult {
+  let result = Command::new("slop").args(&["--color", "0.275,0.510,0.706"]).output().unwrap();
+  let output = String::from_utf8(result.stdout).unwrap();
+  let out_map = output.trim()
+                      .split("\n")
+                      .map(|kv| kv.split("="))
+                      .map(|mut kv| (kv.next().unwrap().into(), kv.next().unwrap().into()))
+                      .collect::<HashMap<String, String>>();
+
+    SlopResult {
+      x: out_map.get("X").unwrap().clone(),
+      y: out_map.get("Y").unwrap().clone(),
+      w: out_map.get("W").unwrap().clone(),
+      h: out_map.get("H").unwrap().clone(),
+      g: out_map.get("G").unwrap().clone(),
+      id: out_map.get("ID").unwrap().clone(),
+      cancel: out_map.get("Cancel").unwrap().clone() == "true",
+    }
+}
+
+fn take_and_crop_screenshot(slop_result: &SlopResult, out_path: &PathBuf) {
+  Command::new("import").args(&["-window", "root", "-crop", &slop_result.g, &out_path.to_string_lossy().into_owned()]).spawn().unwrap().wait();
+}
+
+fn upload_file_to_s3(config: &DropConfig, file_path: &PathBuf) {
+  Command::new("s3cmd")
+    .arg("--force")
+    .arg(format!("--access_key={}", config.aws_key.clone().unwrap()))
+    .arg(format!("--secret_key={}", config.aws_secret.clone().unwrap()))
+    .arg("put")
+    .arg(file_path.to_string_lossy().into_owned())
+    .arg(format!("s3://{}", config.aws_bucket.clone().unwrap()))
+    .output();
+}
+
+fn create_drop_url(config: &DropConfig, filename: String) -> String {
+  match config.drop_host.clone() {
+    Some(host) => format!("http://{}/{}", host, filename),
+    None => format!("http://s3.amazonaws.com/{}/{}", config.aws_bucket.clone().unwrap(), filename)
+  }
+}
+
+fn copy_to_clipboard(url: String) {
+  let mut process = Command::new("xsel").arg("--clipboard").stdin(Stdio::piped()).spawn().unwrap();
+  process.stdin.as_mut().unwrap().write_all(url.as_bytes());
+  process.wait();
+}
+
+fn send_notification(file_path: &PathBuf) {
+  Command::new("notify-send").arg("-i").arg(file_path.to_string_lossy().into_owned()).arg("'Drop url in clipboard.'").spawn().unwrap().wait();
+}
+
+#[derive(Debug)]
+struct SlopResult {
+    x: String,
+    y: String,
+    w: String,
+    h: String,
+    g: String,
+    id: String,
+    cancel: bool,
+}
+
+#[derive(Debug)]
+struct DropConfig {
+  drop_dir: String,
+  drop_host: Option<String>,
+  aws_bucket: Option<String>,
+  aws_key: Option<String>,
+  aws_secret: Option<String>
+}
