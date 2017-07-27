66b2c741b72b4de88ef104e8ea68332c69fd125b
Bryan Gilbert <gilbertw1@gmail.com>
2017-07-27 08:29:26 -0400

    add support for paths in aws bucket configuration

--- src/aws.rs
+++ src/aws.rs
@@ -4,19 +4,15 @@ use std::path::Path;
 use conf::DropConfig;
 
 pub fn upload_file_to_s3(config: &DropConfig, file_path: &Path, file_name: Option<String>) {
+  let object_name = file_name.unwrap_or(file_path.file_name().unwrap().to_string_lossy().into_owned());
   let mut cmd = Command::new("s3cmd");
   cmd.arg("--force")
     .arg("--follow-symlinks")
     .arg(format!("--access_key={}", config.aws_key.clone().unwrap()))
     .arg(format!("--secret_key={}", config.aws_secret.clone().unwrap()))
     .arg("put")
-    .arg(file_path.to_string_lossy().into_owned());
-
-  if file_name.is_some() {
-    cmd.arg(format!("s3://{}/{}", config.aws_bucket.clone().unwrap(), file_name.unwrap()));
-  } else {
-    cmd.arg(format!("s3://{}", config.aws_bucket.clone().unwrap()));
-  }
+    .arg(file_path.to_string_lossy().into_owned())
+    .arg(format!("s3://{}/{}", config.aws_bucket.clone().unwrap(), object_name));
 
   if config.verbose {
     cmd.spawn().unwrap().wait();
